14f876117d644c1d0ee1c9d452d0f95df16e98da
DayoWang <dayowang@hotmail.com>
2019-01-02 22:37:06 +0800

    网关增加ip限流处理

--- uranus-api-gateway-server/src/main/java/me/wgy/ApiGatewayServerApplication.java
+++ uranus-api-gateway-server/src/main/java/me/wgy/ApiGatewayServerApplication.java
@@ -2,7 +2,10 @@ package me.wgy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 /**
  * @author wgy
@@ -14,5 +17,13 @@ public class ApiGatewayServerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ApiGatewayServerApplication.class, args);
+  }
+
+  /**
+   * ip限流
+   */
+  @Bean
+  public KeyResolver ipKeyResolver() {
+    return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
   }
 }
