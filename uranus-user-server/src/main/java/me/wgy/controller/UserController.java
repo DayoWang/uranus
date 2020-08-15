fa9ee353c9b08b5a70e5edc7ae09eea52dd6ccbd
DayoWang <dayowang@hotmail.com>
2020-08-15 19:44:05 +0800

    代码优化

--- uranus-user-server/src/main/java/me/wgy/controller/UserController.java
+++ uranus-user-server/src/main/java/me/wgy/controller/UserController.java
@@ -8,7 +8,6 @@ import me.wgy.log.LoggerManage;
 import me.wgy.model.User;
 import me.wgy.service.UserService;
 import me.wgy.view.Pagination;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,75 +22,78 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RefreshScope
 @RequestMapping("/users")
-@Api(tags = "1.1", description = "用户管理", value = "用户管理")
+@Api(tags = "1.1", value = "用户管理")
 public class UserController {
 
-  @Autowired
-  UserService userService;
+    final UserService userService;
 
-  @Value("${user.message}")
-  String message;
+    @Value("${user.message}")
+    String message;
 
-  @LoggerManage(description = "创建用户")
-  @RequestMapping(value = "/create", method = RequestMethod.POST)
-  @ApiOperation(value = "创建用户")
-  public boolean createUser(@RequestParam(value = "name") String name,
-      @RequestParam(value = "identity") String identity) {
-    User user = new User();
-    user.setName(name);
-    user.setIdentity(identity);
-    try {
-      userService.create(user);
-      return Boolean.TRUE;
-    } catch (Exception e) {
-      return Boolean.FALSE;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
-  }
 
-  @LoggerManage(description = "根据姓名查询用户")
-  @RequestMapping(value = "/query/name", method = RequestMethod.GET)
-  @ApiOperation(value = "根据姓名查询用户")
-  public User queryByName(@RequestParam(value = "name") String name) {
-    return userService.queryByName(name);
-  }
+    @LoggerManage(description = "创建用户")
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    @ApiOperation(value = "创建用户")
+    public boolean createUser(@RequestParam(value = "name") String name,
+                              @RequestParam(value = "identity") String identity) {
+        User user = new User();
+        user.setName(name);
+        user.setIdentity(identity);
+        try {
+            userService.create(user);
+            return Boolean.TRUE;
+        } catch (Exception e) {
+            return Boolean.FALSE;
+        }
+    }
 
-  @LoggerManage(description = "根据证件号查询用户")
-  @RequestMapping(value = "/query/identity", method = RequestMethod.GET)
-  @ApiOperation(value = "根据证件号查询用户")
-  public User queryByIdentity(@RequestParam(value = "identity") String identity) {
-    return userService.queryByIdentity(identity);
-  }
+    @LoggerManage(description = "根据姓名查询用户")
+    @RequestMapping(value = "/query/name", method = RequestMethod.GET)
+    @ApiOperation(value = "根据姓名查询用户")
+    public User queryByName(@RequestParam(value = "name") String name) {
+        return userService.queryByName(name);
+    }
 
-  @LoggerManage(description = "分页获取所有用户")
-  @RequestMapping(value = "/query/users", method = RequestMethod.GET)
-  @ApiOperation(value = "分页获取所有用户")
-  public Pagination<User> queryAll(
-      @RequestParam(value = "pageNum", required = false, defaultValue = "1") Integer pageNum,
-      @RequestParam(value = "pageSize", required = false, defaultValue = "10") Integer pageSize) {
-    return userService.queryAllByPage(pageNum, pageSize);
-  }
+    @LoggerManage(description = "根据证件号查询用户")
+    @RequestMapping(value = "/query/identity", method = RequestMethod.GET)
+    @ApiOperation(value = "根据证件号查询用户")
+    public User queryByIdentity(@RequestParam(value = "identity") String identity) {
+        return userService.queryByIdentity(identity);
+    }
 
-  @LoggerManage(description = "获取消息配置")
-  @SentinelResource(value = "message", blockHandler = "exceptionHandler", fallback = "messageFallback")
-  @RequestMapping(value = "/message", method = RequestMethod.GET)
-  @ApiOperation(value = "获取消息配置")
-  public String message() {
-    return message;
-  }
+    @LoggerManage(description = "分页获取所有用户")
+    @RequestMapping(value = "/query/users", method = RequestMethod.GET)
+    @ApiOperation(value = "分页获取所有用户")
+    public Pagination<User> queryAll(
+            @RequestParam(value = "pageNum", required = false, defaultValue = "1") Integer pageNum,
+            @RequestParam(value = "pageSize", required = false, defaultValue = "10") Integer pageSize) {
+        return userService.queryAllByPage(pageNum, pageSize);
+    }
 
-  /**
-   * Fallback 函数，函数签名与原函数一致.
-   */
-  public String messageFallback() {
-    return String.format("Message Fallback");
-  }
+    @LoggerManage(description = "获取消息配置")
+    @SentinelResource(value = "message", blockHandler = "exceptionHandler", fallback = "messageFallback")
+    @RequestMapping(value = "/message", method = RequestMethod.GET)
+    @ApiOperation(value = "获取消息配置")
+    public String message() {
+        return message;
+    }
 
-  /**
-   * Block 异常处理函数，参数最后多一个 BlockException，其余与原函数一致.
-   */
-  public String exceptionHandler(BlockException ex) {
-    // Do some log here.
-    ex.printStackTrace();
-    return "Message, exceptionHandler ";
-  }
+    /**
+     * Fallback 函数，函数签名与原函数一致.
+     */
+    public String messageFallback() {
+        return "Message Fallback";
+    }
+
+    /**
+     * Block 异常处理函数，参数最后多一个 BlockException，其余与原函数一致.
+     */
+    public String exceptionHandler(BlockException ex) {
+        // Do some log here.
+        ex.printStackTrace();
+        return "Message, exceptionHandler ";
+    }
 }
